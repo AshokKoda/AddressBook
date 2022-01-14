429720d1102fd3e5c1df2133bd5ec892a193afd5
Ashok Koda <a.koda59@gmail.com>
2022-01-14 16:13:40 +0530

    Added:Create a contacts in address book

---
+++ src/main/java/entity/Address.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Address {
+
+	private String city;
+	private String State;
+	private int zip;
+	private long phoneNo;
+}
---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,14 @@
+package entity;
+
+/*
+ * Person Entity
+ */
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Address address;
+	
+	
+}
